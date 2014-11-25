051daad20d76ccf5a80e6989376456fc5ee0e90e
Nik Nyby <nnyby@columbia.edu>
2014-11-25 17:32:44 -0500

    AWS settings

--- worth2/settings_production.py
+++ worth2/settings_production.py
@@ -28,6 +28,21 @@ COMPRESS_ROOT = "/var/www/worth2/worth2/media/"
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+AWS_STORAGE_BUCKET_NAME = "ccnmtl-worth2-static-prod"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'worth2.s3utils.CompressorS3BotoStorage'
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+DEFAULT_FILE_STORAGE = 'worth2.s3utils.MediaRootS3BotoStorage'
+MEDIA_URL = S3_URL + '/media/'
+COMPRESS_STORAGE = 'worth2.s3utils.CompressorS3BotoStorage'
+AWS_QUERYSTRING_AUTH = False
+
 if 'migrate' not in sys.argv:
     INSTALLED_APPS.append('raven.contrib.django.raven_compat')				
 
