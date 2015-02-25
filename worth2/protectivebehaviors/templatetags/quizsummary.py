659f1c2c2e864ba29fdb1a45fc713241f6c3f91a
Nik Nyby <nikolas@gnu.org>
2015-02-24 20:23:15 -0500

    Add customized feedback for the ratemyrisk quiz - PMT #98028
    In the process, I refactored the get_quiz_summary templatetag. I
    simplified it a lot by using the @register.assignment_tag decorator.

--- worth2/protectivebehaviors/templatetags/quizsummary.py
+++ worth2/protectivebehaviors/templatetags/quizsummary.py
@@ -6,7 +6,34 @@ from worth2.main.views import get_quiz_blocks
 register = template.Library()
 
 
-class PositiveQuizResponses(template.Node):
+def get_positive_responses_from_blocks(blocks, user):
+    """
+    Returns the responses to the quizzes in param 'blocks' that have
+    positive values (i.e. greater than 0).
+
+    The returned array is sorted by value, descending.
+    """
+
+    positive_answers = []
+    for b in blocks:
+        # assumption: each of these quiz types has one question
+        latest_submission = Submission.objects.filter(
+            quiz=b.content_object, user=user).order_by('-submitted').first()
+
+        if latest_submission:
+            positive_responses = Response.objects.filter(
+                submission=latest_submission, value__gte=1)
+            for r in positive_responses:
+                positive_answers.append(r)
+
+    sorted_answers = sorted(positive_answers,
+                            key=lambda answer: answer.value,
+                            reverse=True)
+    return sorted_answers
+
+
+@register.assignment_tag
+def get_quiz_summary(user, cls):
     """Aggregate a yes/no quiz by collecting all positive responses.
 
     The Quiz this works on is expected to be implemented as a series of
@@ -14,39 +41,20 @@ class PositiveQuizResponses(template.Node):
     false.
     """
 
-    def __init__(self, user, quiz_class, var_name):
-        self.user = user
-        self.quiz_class = quiz_class
-        self.var_name = var_name
-
-    def render(self, context):
-        u = context[self.user]
-        cls = context[self.quiz_class]
-
-        blocks = get_quiz_blocks(cls)
-
-        positive_answers = []
-        for b in blocks:
-            # assumption: each of these quiz types has one question
-            latest_submission = Submission.objects.filter(
-                quiz=b.content_object, user=u).order_by('-submitted').first()
-
-            if latest_submission:
-                positive_responses = Response.objects.filter(
-                    submission=latest_submission, value__gte=1)
-                for r in positive_responses:
-                    positive_answers.append(r)
-
-        sorted_answers = sorted(positive_answers,
-                                key=lambda answer: answer.value,
-                                reverse=True)
-        context[self.var_name] = sorted_answers
-        return ''
+    return get_positive_responses_from_blocks(get_quiz_blocks(cls), user)
 
 
-@register.tag('get_quiz_summary')
-def quizsummary(parser, token):
-    user = token.split_contents()[1:][0]
-    quiz_class = token.split_contents()[1:][1]
-    var_name = token.split_contents()[1:][3]
-    return PositiveQuizResponses(user, quiz_class, var_name)
+@register.assignment_tag
+def get_aggregate_level(user, cls):
+    """
+    Returns the max value for responses for 'user' on pageblocks associated
+    with the css class 'cls'.
+    """
+
+    responses = get_positive_responses_from_blocks(get_quiz_blocks(cls), user)
+
+    level = 0
+    for r in responses:
+        level = max(r.value, level)
+
+    return int(level)
