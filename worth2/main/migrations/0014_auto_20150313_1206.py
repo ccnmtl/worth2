786ffd32972309fe090f72ddda4f41aeaf7412a8
Nik Nyby <nnyby@columbia.edu>
2015-03-13 12:07:22 -0400

    Add a missing migration
    Commit 4cbb14a6 actually introduced a migration that I didn't add in the PR.

---
+++ worth2/main/migrations/0014_auto_20150313_1206.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0013_auto_20150225_2132'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='participant',
+            name='study_id',
+            field=models.CharField(db_index=True, unique=True, max_length=255, validators=[django.core.validators.RegexValidator(regex=b'^\\d{12}$', message=b"That study ID isn't valid. (It needs to be 12 digits)")]),
+            preserve_default=True,
+        ),
+    ]
