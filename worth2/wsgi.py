ec70d6bd7db3b0a31f047794c72910ee209a61d5
Anders Pearson <anders@columbia.edu>
2015-09-21 13:20:29 +0200

    :arrow_up: pep8 1.6.2. PMT #102575

--- worth2/wsgi.py
+++ worth2/wsgi.py
@@ -1,8 +1,8 @@
 import os
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "worth2.settings")
-
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
