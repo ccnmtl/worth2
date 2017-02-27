f6dcb845f8bed43af0fc5b2cef32908ded5e6bf0
Anders Pearson <anders@columbia.edu>
2017-02-27 16:27:22 +0000

    remove study_id_validator()
    function does nothing. Looks like it was once used for doing some
    validation on a migration, but has since been emptied out. No point in
    keeping it around. Removed the references to it from
    migrations. Obviously, if those migrations run, they won't do whatever
    validation the function used to do, but that's been true ever since it
    was turned into a no-op.

--- worth2/main/migrations/0019_auto_20150701_1544.py
+++ worth2/main/migrations/0019_auto_20150701_1544.py
@@ -17,6 +17,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='participant',
             name='study_id',
-            field=models.CharField(db_index=True, unique=True, max_length=255, validators=[django.core.validators.RegexValidator(regex=b'^[1-2]\\d[0-1]\\d[0-3]\\d\\d\\d[0-2]\\d[0-5]\\d$', message=b"That study ID isn't valid. The format is: YYMMDDLLHHMM (where LL is the two-digit location code)."), worth2.main.models.study_id_validator]),
+            field=models.CharField(db_index=True, unique=True, max_length=255, validators=[django.core.validators.RegexValidator(regex=b'^[1-2]\\d[0-1]\\d[0-3]\\d\\d\\d[0-2]\\d[0-5]\\d$', message=b"That study ID isn't valid. The format is: YYMMDDLLHHMM (where LL is the two-digit location code).")]),
         ),
     ]
