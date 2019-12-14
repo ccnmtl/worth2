11db36370ef340ad2a53a80aff937da4a802bce2
Susan Dreher <sdreher@gmail.com>
2019-12-14 13:05:04 -0500

    retrieve csrf token through the meta tag

--- media/js/src/ssnm/adapters/application.js
+++ media/js/src/ssnm/adapters/application.js
@@ -1,27 +1,10 @@
 (function() {
     'use strict';
-
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(
-                        cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
+    var token = $('meta[name="csrf-token"]').attr('content');
     Ssnm.ApplicationAdapter = DS.JSONAPIAdapter.extend({
         namespace: 'ssnm/api',
         headers: {
-            'X-CSRFToken': getCookie('csrftoken')
+            'X-CSRFToken': token
         }
     });
 })();
